ec23c84e7dc25f7031d5e649d02b677197137e3e
Richie-Z <zakariarichie@yahoo.com>
2023-06-01 01:38:14 +0700

    nvim tree hjkl style

--- configs/overrides.lua
+++ configs/overrides.lua
@@ -58,6 +58,42 @@ M.nvimtree = {
     local ok, api = pcall(require, "nvim-tree.api")
     assert(ok, "api module is not found")
     vim.keymap.set("n", "<CR>", api.node.open.tab_drop, opts "Tab drop")
+
+    local function edit_or_open()
+      local node = api.tree.get_node_under_cursor()
+
+      if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+      else
+        -- open file
+        api.node.open.edit()
+        -- Close the tree if file was opened
+        api.tree.close()
+      end
+    end
+
+    -- open as vsplit on current node
+    local function vsplit_preview()
+      local node = api.tree.get_node_under_cursor()
+
+      if node.nodes ~= nil then
+        -- expand or collapse folder
+        api.node.open.edit()
+      else
+        -- open file as vsplit
+        api.node.open.vertical()
+      end
+
+      api.tree.close()
+    end
+
+    vim.keymap.set("n", "l", edit_or_open, opts "Edit Or Open")
+    vim.keymap.set("n", "L", vsplit_preview, opts "Open: Vertical Split")
+
+    vim.keymap.set("n", "h", api.node.navigate.parent_close, opts "Close")
+    vim.keymap.set("n", "H", api.tree.collapse_all, opts "Collapse All")
+    vim.keymap.set("n", "C", api.tree.change_root_to_node, opts "CD")
   end,
 }
 
